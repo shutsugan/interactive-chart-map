da4557b778c6e05c0f465dd2c640a74b5b0f64e3
shutsugan <shutsugan@gmail.com>
2018-07-16 23:24:55 +0100

    Implemented the rest the MapChart class, added styles

--- main.js
+++ main.js
@@ -13,12 +13,61 @@ class MapChart {
 	}
 
 	_addEventListener() {
-		this._paths.forEach(path => path.addEventListener('mouseenter', this._OverMap));
+		this._paths.forEach(path => path.addEventListener('mouseenter', this._overMap));
+		this._links.forEach(link => link.addEventListener('mouseenter', this._overLink));
+
+		this._paths.forEach(path => path.addEventListener('mouseout', this._outMap));
+		this._links.forEach(link => link.addEventListener('mouseout', this._outLink));
 	}
 
-	_OverMap(event) {
-		const city = event.target.getAttribute('title');
-		console.log(city);
+	/**
+     * Hover over map will target the badge city.
+     * @param {event} event
+	 */
+	_overMap(event) {
+		const link = MapChart._getLink(event);
+		link.classList.add('link-active');
 	}
-}
+
+	/**
+     * Hover out of map will clear the badge.
+     * @param {event} event
+	 */
+	_outMap(event) {
+		const link = MapChart._getLink(event);
+		link.classList.remove('link-active');
+	}
+
+	/**
+     * Hover over badge will target the same area.
+     * @param {event} event
+	 */
+	_overLink(event) {
+		const area = MapChart._getArea(event);
+		area.style.fill = '#ec8f07';
+	}
+
+	/**
+     * Hover out of badge will clear area.
+     * @param {event} event
+	 */
+	_outLink(event) {
+		const area = MapChart._getArea(event);
+		area.style.fill = '#4B4B4B';
+	}
+
+	static _getLink(event) {
+		const city = event.target.getAttribute('title');
+		const link = document.querySelector(`.${city}`);
+
+		return link;
+	}
+
+	static _getArea(event) {
+		const city = event.target.innerHTML;
+		const area = document.querySelector(`[title=${city}]`);
+
+		return area;
+	}
+} 
 new MapChart();
